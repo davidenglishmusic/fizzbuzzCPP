ea83e85e04d95698b7700f55811593b7737202d9
David English <david@operaonfilm.com>
2014-11-20 08:33:25 -0800

    The program now prompts the user for a number representing the top limit

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -3,15 +3,20 @@ using namespace std;
 
 int main()
 {
-  for ( int x = 0; x < 100; x++){
+  int y;
+
+  cout << "Enter a number:";
+  cin >> y;
+
+  for ( int x = 1; x <= y; x++){
     if (x % 15 == 0){
-      cout<<"fizzbuzz\n";
+      cout << "fizzbuzz\n";
     }
     else if (x % 5 == 0){
-      cout<<"buzz\n";
+      cout << "buzz\n";
     }
     else if (x % 3 == 0){
-      cout<<"fizz\n";
+      cout << "fizz\n";
     }
     else{
       cout<< x << "\n";
